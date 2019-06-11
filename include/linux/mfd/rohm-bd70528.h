e738a9ecd051d2bdd5649158e70ec87c1560f5a2
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2019-06-11 10:17:28 +0300

    fix compilation

--- include/linux/mfd/rohm-bd70528.h
+++ include/linux/mfd/rohm-bd70528.h
@@ -4,7 +4,7 @@
 #ifndef __LINUX_MFD_BD70528_H__
 #define __LINUX_MFD_BD70528_H__
 
-#include <linux/bits.h>
+//#include <linux/bits.h>
 #include <linux/device.h>
 #include <linux/mfd/rohm-generic.h>
 #include <linux/regmap.h>
