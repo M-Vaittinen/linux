2268e68a236bee165b5af245754249fa93ce4669
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-02-13 12:57:35 +0200

    extconn: Clean-up few drivers by using managed work init
    Few drivers implement remove call-back only for ensuring a delayed
    work gets cancelled prior driver removal. Clean-up these by switching
    to use devm_delayed_work_autocancel() instead.
    
    This change is compile-tested only. All testing is appreciated.
    
    Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>

--- drivers/extcon/extcon-qcom-spmi-misc.c
+++ drivers/extcon/extcon-qcom-spmi-misc.c
@@ -80,7 +80,11 @@ static int qcom_usb_extcon_probe(struct platform_device *pdev)
 	}
 
 	info->debounce_jiffies = msecs_to_jiffies(USB_ID_DEBOUNCE_MS);
-	INIT_DELAYED_WORK(&info->wq_detcable, qcom_usb_extcon_detect_cable);
+
+	ret = devm_delayed_work_autocancel(dev, &info->wq_detcable,
+					   qcom_usb_extcon_detect_cable);
+	if (ret)
+		return ret;
 
 	info->irq = platform_get_irq_byname(pdev, "usb_id");
 	if (info->irq < 0)
@@ -101,15 +105,6 @@ static int qcom_usb_extcon_probe(struct platform_device *pdev)
 
 	/* Perform initial detection */
 	qcom_usb_extcon_detect_cable(&info->wq_detcable.work);
-
-	return 0;
-}
-
-static int qcom_usb_extcon_remove(struct platform_device *pdev)
-{
-	struct qcom_usb_extcon_info *info = platform_get_drvdata(pdev);
-
-	cancel_delayed_work_sync(&info->wq_detcable);
 
 	return 0;
 }
@@ -149,7 +144,6 @@ MODULE_DEVICE_TABLE(of, qcom_usb_extcon_dt_match);
 
 static struct platform_driver qcom_usb_extcon_driver = {
 	.probe		= qcom_usb_extcon_probe,
-	.remove		= qcom_usb_extcon_remove,
 	.driver		= {
 		.name	= "extcon-pm8941-misc",
 		.pm	= &qcom_usb_extcon_pm_ops,
