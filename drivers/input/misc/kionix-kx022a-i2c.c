b3fdfd0928a7bc69320b9350b9caea4dc59b93f5
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2022-06-02 16:04:13 +0300

    input: misc: kx022a: kx022 => kx022a

--- drivers/input/misc/kionix-kx022a-i2c.c
+++ drivers/input/misc/kionix-kx022a-i2c.c
@@ -12,7 +12,7 @@
 
 #include "kionix-kx022a.h"
 
-static int kx022_i2c_probe(struct i2c_client *i2c)
+static int kx022a_i2c_probe(struct i2c_client *i2c)
 {
 	struct regmap *regmap;
 	struct device *dev = &i2c->dev;
@@ -22,37 +22,37 @@ static int kx022_i2c_probe(struct i2c_client *i2c)
 		return -EINVAL;
 	}
 
-	regmap = devm_regmap_init_i2c(i2c, &kx022_regmap);
+	regmap = devm_regmap_init_i2c(i2c, &kx022a_regmap);
 	if (IS_ERR(regmap)) {
 		dev_err(dev, "Failed to initialize Regmap\n");
 
 		return PTR_ERR(regmap);
 	}
 
-	return kx022_probe_internal(dev, i2c->irq, BUS_I2C);
+	return kx022a_probe_internal(dev, i2c->irq, BUS_I2C);
 }
 
-static const struct of_device_id kx022_of_match[] = {
+static const struct of_device_id kx022a_of_match[] = {
 	{ .compatible = "kionix,kx022a-i2c", },
 	{ },
 };
-MODULE_DEVICE_TABLE(of, kx022_of_match);
+MODULE_DEVICE_TABLE(of, kx022a_of_match);
 /*
-static const struct dev_pm_ops kx022_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(kx022_suspend, kx022_resume)
-	SET_RUNTIME_PM_OPS(kx022_runtime_suspend, kx022_runtime_resume, NULL)
+static const struct dev_pm_ops kx022a_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(kx022a_suspend, kx022a_resume)
+	SET_RUNTIME_PM_OPS(kx022a_runtime_suspend, kx022a_runtime_resume, NULL)
 };
 */
-static struct i2c_driver kx022_i2c_driver = {
+static struct i2c_driver kx022a_i2c_driver = {
 	.driver = {
 			.name  = "kx022a-i2c",
-//			.pm    = &kx022_pm_ops,
-			.of_match_table = kx022_of_match,
+//			.pm    = &kx022a_pm_ops,
+			.of_match_table = kx022a_of_match,
 		  },
-	.probe_new    = kx022_i2c_probe,
+	.probe_new    = kx022a_i2c_probe,
 };
 
-module_i2c_driver(kx022_i2c_driver);
+module_i2c_driver(kx022a_i2c_driver);
 
 MODULE_DESCRIPTION("ROHM/Kionix KX022A accelerometer driver");
 MODULE_AUTHOR("Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>");
--- drivers/input/misc/kionix-kx022a-spi.c
+++ drivers/input/misc/kionix-kx022a-spi.c
@@ -12,7 +12,7 @@
 
 #include "kionix-kx022a.h"
 
-static int kx022_spi_probe(struct spi_device *spi)
+static int kx022a_spi_probe(struct spi_device *spi)
 {
 	struct device *dev = &spi->dev;
 	struct regmap *regmap;
@@ -22,35 +22,35 @@ static int kx022_spi_probe(struct spi_device *spi)
 		return -EINVAL;
 	}
 
-	regmap = devm_regmap_init_spi(spi, &kx022_regmap);
+	regmap = devm_regmap_init_spi(spi, &kx022a_regmap);
 	if (IS_ERR(regmap)) {
 		dev_err(dev, "Failed to initialize Regmap\n");
 
 		return PTR_ERR(regmap);
 	}
-	return kx022_probe_internal(dev, spi->irq, BUS_SPI);
+	return kx022a_probe_internal(dev, spi->irq, BUS_SPI);
 }
 
-static const struct of_device_id kx022_of_match[] = {
+static const struct of_device_id kx022a_of_match[] = {
 	{ .compatible = "rohm,kx022a-spi", },
 	{ },
 };
-MODULE_DEVICE_TABLE(of, kx022_of_match);
+MODULE_DEVICE_TABLE(of, kx022a_of_match);
 /*
-static const struct dev_pm_ops kx022_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(kx022_suspend, kx022_resume)
-	SET_RUNTIME_PM_OPS(kx022_runtime_suspend, kx022_runtime_resume, NULL)
+static const struct dev_pm_ops kx022a_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(kx022a_suspend, kx022a_resume)
+	SET_RUNTIME_PM_OPS(kx022a_runtime_suspend, kx022a_runtime_resume, NULL)
 };
 */
-static struct spi_driver kx022_spi_driver = {
+static struct spi_driver kx022a_spi_driver = {
 	.driver = {
 		.name   = "kx022a-spi",
-//		.pm     = &kx022_spi_pm,
+//		.pm     = &kx022a_spi_pm,
 	},
-	.probe	= kx022_spi_probe,
+	.probe	= kx022a_spi_probe,
 };
 
-module_spi_driver(kx022_spi_driver);
+module_spi_driver(kx022a_spi_driver);
 
 MODULE_DESCRIPTION("ROHM/Kionix kx022A accelerometer driver");
 MODULE_AUTHOR("Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>");
