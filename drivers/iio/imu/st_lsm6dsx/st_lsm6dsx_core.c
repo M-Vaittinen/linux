b3dc1ff6801ac7d23d6cbf928421878f6ceb2363
Matti Vaittinen <mazziesaccount@gmail.com>
2022-08-15 08:04:30 +0300

    iio: st_lsm6dsx: Simplify using devm_regulator_*get_enable()
    Use devm_regulator_bulk_get_enable() instead of open coded bulk-get,
    bulk-enable, add-action-to-disable-at-detach - pattern.
    
    A functional change (which seems like a bugfix) is that if
    regulator_bulk_get fails, the enable is not attempted.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    
    ---
    v2 => v3
    Split to own patch.

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_core.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_core.c
@@ -2172,34 +2172,18 @@ static int st_lsm6dsx_irq_setup(struct st_lsm6dsx_hw *hw)
 
 static int st_lsm6dsx_init_regulators(struct device *dev)
 {
-	struct st_lsm6dsx_hw *hw = dev_get_drvdata(dev);
 	int err;
-
 	/* vdd-vddio power regulators */
-	hw->regulators[0].supply = "vdd";
-	hw->regulators[1].supply = "vddio";
-	err = devm_regulator_bulk_get(dev, ARRAY_SIZE(hw->regulators),
-				      hw->regulators);
-	if (err)
-		return dev_err_probe(dev, err, "failed to get regulators\n");
+	static const char * const regulators[] = {"vdd", "vddio"};
 
-	err = regulator_bulk_enable(ARRAY_SIZE(hw->regulators),
-				    hw->regulators);
-	if (err) {
-		dev_err(dev, "failed to enable regulators: %d\n", err);
-		return err;
-	}
+	err = devm_regulator_bulk_get_enable(dev, ARRAY_SIZE(regulators),
+					     regulators);
+	if (err)
+		return dev_err_probe(dev, err, "failed to enable regulators\n");
 
 	msleep(50);
 
 	return 0;
-}
-
-static void st_lsm6dsx_chip_uninit(void *data)
-{
-	struct st_lsm6dsx_hw *hw = data;
-
-	regulator_bulk_disable(ARRAY_SIZE(hw->regulators), hw->regulators);
 }
 
 int st_lsm6dsx_probe(struct device *dev, int irq, int hw_id,
@@ -2222,10 +2206,6 @@ int st_lsm6dsx_probe(struct device *dev, int irq, int hw_id,
 	mutex_init(&hw->page_lock);
 
 	err = st_lsm6dsx_init_regulators(dev);
-	if (err)
-		return err;
-
-	err = devm_add_action_or_reset(dev, st_lsm6dsx_chip_uninit, hw);
 	if (err)
 		return err;
 
