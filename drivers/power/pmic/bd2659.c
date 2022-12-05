0ae802aa16aaf05e0faeb9cdd8913c5e062fcf81
Matti Vaittinen <mazziesaccount@gmail.com>
2022-12-05 16:13:01 +0200

    pmic: bd2659: Use common functions

--- drivers/power/pmic/bd2659.c
+++ drivers/power/pmic/bd2659.c
@@ -13,6 +13,7 @@
 #include <power/pmic.h>
 #include <power/regulator.h>
 #include <power/bd2659.h>
+#include "bdxxxx.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -24,53 +25,14 @@ static const struct pmic_child_info pmic_children_info[] = {
 	{ },
 };
 
+static int bd2659_bind(struct udevice *dev)
+{
+	return bdxxxx_bind(dev, pmic_children_info);
+}
+
 static int bd2659_reg_count(struct udevice *dev)
 {
 	return BD2659_MAX_REGISTER - 1;
-}
-
-static int bd2659_write(struct udevice *dev, uint reg, const uint8_t *buff,
-			 int len)
-{
-	if (dm_i2c_write(dev, reg, buff, len)) {
-		pr_err("write error to device: %p register: %#x!", dev, reg);
-		return -EIO;
-	}
-
-	return 0;
-}
-
-static int bd2659_read(struct udevice *dev, uint reg, uint8_t *buff, int len)
-{
-	if (dm_i2c_read(dev, reg, buff, len)) {
-		pr_err("read error from device: %p register: %#x!", dev, reg);
-		return -EIO;
-	}
-
-	return 0;
-}
-
-static int bd2659_bind(struct udevice *dev)
-{
-	int children;
-	ofnode regulators_node;
-
-	regulators_node = dev_read_subnode(dev, "regulators");
-	if (!ofnode_valid(regulators_node)) {
-		debug("%s: %s regulators subnode not found!\n", __func__,
-		      dev->name);
-		return -ENXIO;
-	}
-
-	debug("%s: '%s' - found regulators subnode\n", __func__, dev->name);
-
-	if (CONFIG_IS_ENABLED(PMIC_CHILDREN)) {
-		children = pmic_bind_children(dev, regulators_node, pmic_children_info);
-		if (!children)
-			debug("%s: %s - no child found\n", __func__, dev->name);
-	}
-	/* Always return success for this device */
-	return 0;
 }
 
 static int bd2659_probe(struct udevice *dev)
@@ -91,8 +53,8 @@ static int bd2659_probe(struct udevice *dev)
 
 static struct dm_pmic_ops bd2659_ops = {
 	.reg_count = bd2659_reg_count,
-	.read = bd2659_read,
-	.write = bd2659_write,
+	.read = bdxxxx_read,
+	.write = bdxxxx_write,
 };
 
 static const struct udevice_id bd2659_ids[] = {
