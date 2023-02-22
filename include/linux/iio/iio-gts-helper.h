b5409a6af71cec785909bbb8dff6d1602c45ab5d
Matti Vaittinen <mazziesaccount@gmail.com>
2023-02-22 09:56:02 +0200

    iio: light: Add gain-time-scale helpers
    Some light sensors can adjust both the HW-gain and integration time.
    There are cases where adjusting the integration time has similar impact
    to the scale of the reported values as gain setting has.
    
    IIO users do typically expect to handle scale by a single writable 'scale'
    entry. Driver should then adjust the gain/time accordingly.
    
    It however is difficult for a driver to know whether it should change
    gain or integration time to meet the requested scale. Usually it is
    preferred to have longer integration time which usually improves
    accuracy, but there may be use-cases where long measurement times can be
    an issue. Thus it can be preferable to allow also changing the
    integration time - but mitigate the scale impact by also changing the gain
    underneath. Eg, if integration time change doubles the measured values,
    the driver can reduce the HW-gain to half.
    
    The theory of the computations of gain-time-scale is simple. However,
    some people (undersigned) got that implemented wrong for more than once.
    
    Add some gain-time-scale helpers in order to not dublicate errors in all
    drivers needing these computations.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    
    ---
    Currently it is only BU27034 using these in this series. I am however working
    with drivers for RGB sensors BU27008 and BU27010 which have similar
    [gain - integration time - scale] - relation. I hope sending those
    follows soon after the BU27034 is done.
    
    Changes:
    v3 => v4:
    - doc styling
    - use memset to zero the helper struct at init
    - drop unnecessary min calculation at iio_find_closest_gain_low()
    - use namespace to all exports
    - many minor stylings
    - make available outside iio/light (move code to drivers/iio and move the
      header under include
    - rename to look like other files under drivers/iio (s/iio/industrialio)
    
    v2 => v3: (mostly fixes based on review by Andy)
    - Fix typos
    - Styling fixes
    - Use namespace for exported symbols
    - Protect allocs against argument overflow
    - Fix include protection name
    - add types.h inclusion and struct device forward declaration
    
    RFCv1 => v2:
    - fix include guardian
    - Improve kernel doc for iio_init_iio_gts.
    - Add iio_gts_scale_to_total_gain
    - Add iio_gts_total_gain_to_scale
    - Fix review comments from Jonathan
      - add documentation to few functions
      - replace 0xffffffffffffffffLLU by U64_MAX
      - some styling fixes
      - drop unnecessary NULL checks
      - order function arguments by  in / out purpose
      - drop GAIN_SCALE_ITIME_MS()
    - Add helpers for available scales and times
    - Rename to iio-gts-helpers

---
+++ include/linux/iio/iio-gts-helper.h
@@ -0,0 +1,134 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* gain-time-scale conversion helpers for IIO light sensors
+ *
+ * Copyright (c) 2023 Matti Vaittinen <mazziesaccount@gmail.com>
+ */
+
+#ifndef __IIO_GTS_HELPER__
+#define __IIO_GTS_HELPER__
+
+#include <linux/types.h>
+
+struct device;
+
+/**
+ * struct iio_gain_sel_pair - gain - selector values
+ *
+ * In many cases devices like light sensors allow setting signal amplification
+ * (gain) using a register interface. This structure describes amplification
+ * and corresponding selector (register value)
+ *
+ * @gain:	Gain (multiplication) value.
+ * @sel:	Selector (usually register value) used to indicate this gain
+ */
+struct iio_gain_sel_pair {
+	int gain;
+	int sel;
+};
+
+/**
+ * struct iio_itime_sel_mul - integration time description
+ *
+ * In many cases devices like light sensors allow setting the duration of
+ * collecting data. Typically this duration has also an impact to the magnitude
+ * of measured values (gain). This structure describes the relation of
+ * integration time and amplification as well as corresponding selector
+ * (register value).
+ *
+ * An example could be a sensor allowing 50, 100, 200 and 400 mS times. The
+ * respective multiplication values could be 50 mS => 1, 100 mS => 2,
+ * 200 mS => 4 and 400 mS => 8 assuming the impact of integration time would be
+ * linear in a way that when collecting data for 50 mS caused value X, doubling
+ * the data collection time caused value 2X etc..
+ *
+ * @time_us:	Integration time in microseconds.
+ * @sel:	Selector (usually register value) used to indicate this time
+ * @mul:	Multiplication to the values caused by this time.
+ */
+struct iio_itime_sel_mul {
+	int time_us;
+	int sel;
+	int mul;
+};
+
+struct iio_gts {
+	u64 max_scale;
+	const struct iio_gain_sel_pair *hwgain_table;
+	int num_hwgain;
+	const struct iio_itime_sel_mul *itime_table;
+	int num_itime;
+	int **per_time_avail_scale_tables;
+	int *avail_all_scales_table;
+	int num_avail_all_scales;
+	int *avail_time_tables;
+	int num_avail_time_tables;
+};
+
+#define GAIN_SCALE_GAIN(_gain, _sel)			\
+{							\
+	.gain = (_gain),				\
+	.sel = (_sel),					\
+}
+
+#define GAIN_SCALE_ITIME_US(_itime, _sel, _mul)		\
+{							\
+	.time_us = (_itime),				\
+	.sel = (_sel),					\
+	.mul = (_mul),					\
+}
+
+int iio_init_iio_gts(int max_scale_int, int max_scale_nano,
+		     const struct iio_gain_sel_pair *gain_tbl, int num_gain,
+		     const struct iio_itime_sel_mul *tim_tbl, int num_times,
+		     struct iio_gts *gts);
+
+bool iio_gts_valid_gain(struct iio_gts *gts, int gain);
+bool iio_gts_valid_time(struct iio_gts *gts, int time_us);
+
+int iio_gts_get_total_gain_by_sel(struct iio_gts *gts, int gsel, int tsel);
+int iio_gts_get_total_gain(struct iio_gts *gts, int gain, int time);
+
+int iio_find_closest_gain_low(struct iio_gts *gts, int gain, bool *in_range);
+int iio_gts_find_gain_by_sel(struct iio_gts *gts, int sel);
+int iio_gts_find_sel_by_gain(struct iio_gts *gts, int gain);
+int iio_gts_get_min_gain(struct iio_gts *gts);
+int iio_gts_find_int_time_by_sel(struct iio_gts *gts, int sel);
+int iio_gts_find_sel_by_int_time(struct iio_gts *gts, int time);
+
+int iio_gts_get_int_time_gain_multiplier_by_sel(struct iio_gts *gts,
+						       int sel);
+int iio_gts_total_gain_to_scale(struct iio_gts *gts, int total_gain,
+				int *scale_int, int *scale_nano);
+int iio_gts_scale_to_total_gain(struct iio_gts *gts, int scale_int,
+				int scale_nano, int *gain_tot);
+int iio_gts_find_gain_sel_for_scale_using_time(struct iio_gts *gts, int time_sel,
+					       int scale_int, int scale_nano,
+					       int *gain_sel);
+int iio_gts_find_gain_for_scale_using_time(struct iio_gts *gts, int time_sel,
+					   int scale_int, int scale_nano,
+					   int *gain);
+int iio_gts_find_time_and_gain_sel_for_scale(struct iio_gts *gts, int scale_int,
+					     int scale_nano, int *gain_sel,
+					     int *time_sel);
+int iio_gts_get_scale(struct iio_gts *gts, int gain, int time, int *scale_int,
+		      int *scale_nano);
+int iio_gts_find_new_gain_sel_by_old_gain_time(struct iio_gts *gts,
+					       int old_gain, int old_time_sel,
+					       int new_time_sel, int *new_gain);
+int iio_gts_build_avail_tables(struct iio_gts *gts);
+int devm_iio_gts_build_avail_tables(struct device *dev, struct iio_gts *gts);
+int iio_gts_build_avail_scale_table(struct iio_gts *gts);
+int devm_iio_gts_build_avail_scale_table(struct device *dev, struct iio_gts *gts);
+int iio_gts_build_avail_time_table(struct iio_gts *gts);
+int devm_iio_gts_build_avail_time_table(struct device *dev, struct iio_gts *gts);
+void iio_gts_purge_avail_scale_table(struct iio_gts *gts);
+void iio_gts_purge_avail_time_table(struct iio_gts *gts);
+void iio_gts_purge_avail_tables(struct iio_gts *gts);
+int iio_gts_avail_times(struct iio_gts *gts,  const int **vals, int *type,
+			int *length);
+int iio_gts_all_avail_scales(struct iio_gts *gts, const int **vals, int *type,
+			     int *length);
+int iio_gts_avail_scales_for_time(struct iio_gts *gts, int time,
+				  const int **vals, int *type, int *length);
+
+#endif
