09fbc5ae738133852485a972c89d0a9364185c9d
Matti Vaittinen <mazziesaccount@gmail.com>
2025-01-27 11:03:40 +0200

    web: move style from db.php to header.php
    The db.php containing all the page-header stuff is counter intuitive
    and confusing. Additionally, updating the db.php is a bit of a pain as
    it is necessary to clean the passwords etc. from the db.php while
    pushing the updated version to git.
    
    Keeping the mysql connection stuff in db.php file alone makes the need
    to update the db.php less frequent.
    
    Create a new 'header.php'-file which contains the header stuff and leave
    the db.php to only contain the database stuff.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- web/include/db.php
+++ web/include/db.php
@@ -14,68 +14,11 @@ $servername = "host";
 $username = "username";
 $password = "password";
 $dbname = "database";
-echo '
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-  background-color: linen;
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
 }
 
-h1 {
-  color: maroon;
-  margin-left: 40px;
-}
-
-table.structure {
-  border: none;
-  border-spaceing: 18px;
-  text-align: left;
-  vertical-align: top;
-}
-.structure td {
-  border: none;
-  text-align: left;
-  vertical-align: top;
-}
-.structure th {
-}
-.structure {
-  border: none;
-  text-align: left;
-  vertical-align: top;
-}
-table.cardlist {
-  border: 1px solid black;
-  border-radius: 10px;
-  border-collapse: collapse;
-}
-
-.cardlist th {
-  border: 1px solid black;
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: center;
-  vertical-align: top;
-  background-color: #d2691e;
-  color: white;
-  border-collapse: collapse;
-}
-
-.cardlist td {
-  border: 1px solid black;
-  border-collapse: collapse;
-  padding-top: 12px;
-  padding-bottom: 12px;
-  vertical-align: top;
-  text-align: center;
-  background-color: ##ffdead;
-  color: brown;
-  border-radius: 10px;
-}
-
-</style>
-</head>
-<body>';
 ?>
---
+++ web/include/header.php
@@ -0,0 +1,193 @@
+<?php
+function do_head($title)
+{
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: linen;
+}
+
+h1 {
+  color: maroon;
+  margin-left: 40px;
+}
+
+table.structure {
+  border: none;
+  border-spaceing: 18px;
+  text-align: left;
+  vertical-align: top;
+}
+.structure td {
+  border: none;
+  text-align: left;
+  padding-left: 30px;
+  padding-right: 30px;
+  vertical-align: top;
+}
+.structure th {
+  text-align: left;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+.structure {
+  border: none;
+  text-align: left;
+  vertical-align: top;
+}
+table.cardlist {
+  border: 1px solid black;
+  border-radius: 10px;
+  border-collapse: collapse;
+}
+
+.cardlist th {
+  border: 1px solid black;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: center;
+  vertical-align: top;
+  background-color: #d2691e;
+  color: white;
+  border-collapse: collapse;
+}
+
+.cardlist td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  vertical-align: top;
+  text-align: center;
+  background-color: #ffdead;
+  color: brown;
+  border-radius: 10px;
+}
+
+table.aarvonta {
+	border: none;
+	width: 100%;
+}
+.aarvonta th {
+  height: 70px;
+  text-align: center;
+  vertical-align: top;
+  background-color: #d2691e;
+  color: white;
+  border-collapse: collapse;
+}
+
+.aarvonta td {
+  border: none;
+  border-collapse: collapse;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  vertical-align: top;
+  text-align: center;
+  background-color: ##ffdead;
+  color: brown;
+  border-radius: 10px;
+}
+
+.help-tip{
+/*    position: absolute;
+    top: 18px;
+    right: 18px; */
+    text-align: center;
+    background-color: #BCDBEA;
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    font-size: 14px;
+    line-height: 26px;
+    cursor: default;
+}
+
+.help-tip:before{
+    content:\'?\';
+    font-weight: bold;
+    color:#fff;
+}
+
+.help-tip:hover, .help-tip:focus, .help-tip:active p{
+    display:block;
+    transform-origin: 100% 0%;
+
+    -webkit-animation: fadeIn 0.3s ease-in-out;
+    animation: fadeIn 0.3s ease-in-out;
+
+}
+
+.help-tip p{    /* The tooltip */
+    display: none;
+    text-align: left;
+    background-color: #1E2021;
+    padding: 20px;
+    width: 300px;
+    border-radius: 3px;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.2);
+    z-index: 0;
+    position:relative;
+/*    position: absolute;
+    right: -4px; */
+    color: #FFF;
+    font-size: 13px;
+    line-height: 1.4;
+}
+
+.help-tip p:before{ /* The pointer of the tooltip */
+    position: absolute;
+    content: \'\';
+    width:0;
+    height: 0;
+    border:6px solid transparent;
+    border-bottom-color:#1E2021;
+    left:10px;
+    top:-12px;
+}
+
+.help-tip p:after{ /* Prevents the tooltip from being hidden */
+    width:100%;
+    height:40px;
+    content:\'\';
+    position: absolute;
+    top:-40px;
+    left:0;
+}
+
+/* CSS animation */
+
+@-webkit-keyframes fadeIn {
+    0% { 
+        opacity:0; 
+        transform: scale(0.6);
+    }
+
+    100% {
+        opacity:100%;
+        transform: scale(1);
+    }
+}
+
+@keyframes fadeIn {
+    0% { opacity:0; }
+    100% { opacity:100%; }
+}
+
+</style>
+<title>' . $title . '</title>
+</head>
+<body>';
+}
+
+/* Stolen from the web https://www.geeksforgeeks.org/how-to-detect-a-mobile-device-using-php/ */
+function isMobileDevice() { 
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo 
+|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i" 
+, $_SERVER["HTTP_USER_AGENT"]); 
+} 
+
+?>
