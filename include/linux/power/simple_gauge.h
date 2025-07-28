c88ff3124bec5b575d8841cd3bc058e62d8ba8e6
Matti Vaittinen <mazziesaccount@gmail.com>
2025-07-28 10:22:10 +0300

    power: supply: simple-gauge: staticize SIMPLE_GAUGE_PROPS and mark unused
    The SIMPLE_GAUGE_PROPS can be made static to limit the scope of the
    symbol. It then also needs to be marked as potentially unused because
    not all of the users of the simple_gauge.h header refer it - which would
    generate a warning if SIMPLE_GAUGE_PROPS was not marked unused.
    
    Make SIMPLE_GAUGE_PROPS static and use GCC attribute ((unused)) to mark
    the SIMPLE_GAUGE_PROPS as potentially unused.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- include/linux/power/simple_gauge.h
+++ include/linux/power/simple_gauge.h
@@ -16,7 +16,7 @@
 #define SW_GAUGE_CLAMP_SOC BIT(3)
 
 /* Power supply properties handled by simple_gauge */
-const enum power_supply_property SIMPLE_GAUGE_PROPS[] = {
+static const enum power_supply_property SIMPLE_GAUGE_PROPS[] __attribute__((unused)) = {
 	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_CYCLE_COUNT,
 	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
