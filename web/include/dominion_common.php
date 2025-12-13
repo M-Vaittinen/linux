53c3d2df7a56fda66d4d3f99c6368ccaf4565bc9
Matti Vaittinen <mazziesaccount@gmail.com>
2025-12-13 12:40:25 +0200

    dominion: Add function for form creation
    The randomizer contains a form where user can input the extensions to
    use, as well as the tuhina, tupina and kapita -preferences. Split this
    code into own function, which can be used by the new randomizer.
    
    NOTE: The new randomizer allows user to pick the tuhina, tupina and
    capita-meters without a check-box. Value '0' is no longer 'remove cards
    with tuhina/tupina/kapita' - but means "no preference, randomize without
    weighing". Hence the form is different for old and new randomizer, and
    the old randomizer still implements this form in index.php. Eg, old
    randomizer does not use this function.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- web/include/dominion_common.php
+++ web/include/dominion_common.php
@@ -69,5 +69,91 @@ function SQL_add_expansion_where($expansion_key, $expansion_id_array)
 	return $expansion_where;
 }
 
+function output_input_form($conn, $mobile, $exp)
+{
+	$result = get_expansions($conn, false);
+
+	/* Output the form table */
+	if (!$mobile) {
+		$output = '<table class="structure"><tr><th>Käytettävät lisäosat</th><th>Tuhina\'o-meter</th> <th>Tupina\'o-meter</th><th>Kapita\'o-meter</th></tr><tr><td>';
+		} else {
+		$output = '<table class="structure"><tr><th>Käytettävät lisäosat</th><th>Painotukset</th></tr><tr><td>';
+		}
+	$output .= '<form action="" method="post">';
+
+	/* Print expansion checkboxes */
+	$tmp_exp_idx = 0;
+	while ($row = mysqli_fetch_assoc($result)) {
+		if (isset($exp) && $exp && $row['id'] == $exp[$tmp_exp_idx]) {
+			$checked = " checked";
+			$tmp_exp_idx ++;
+		} else {
+			$checked = "";
+		}
+	
+		if (!is_numeric($row['id']))
+			die("bad data in database - expansion id");
+	
+		$output .= '<input type="checkbox" id="' . htmlspecialchars($row['name']) . '" name="expansion[]" value="' . $row['id'] . '"'."$checked>";
+		$output .= '<label for="' . htmlspecialchars($row['name']) . '">' . htmlspecialchars($row['name']) . '</label><br>';
+	}
+	$output .= '</td>';
+
+
+	/* ...Tuhina cell: */
+	$output .= '<td>';
+	if ($mobile)
+		$output .= '<b>Tuhina\'o-meter</b> <br />';
+	$output .= '<div class="slidecontainer">
+	  -10<input type="range" min="-10" max="10" value="0" class="slider" name="tuhinarange" id="tuhinarange">+10
+	</div>';
+	$output .= '<div class="help-tip">
+	    <p>Tuhina\'o-meter&copy; :ll&auml; voit muuttaa korttiarvontaa v&auml;hent&auml;m&auml;&auml;n tai lis&auml;&auml;m&auml;&auml;n toimintoketjuja lis&auml;&auml;vi&auml; kortteja.</p>
+	</div>';
+	$output .= '</td>';
+	
+	if ($mobile) {
+		/* On a mobile we end the row here */
+		$output .= '</tr><tr><td></td>';
+	}
+
+	/* Tupina cell: */
+	$output .= '<td>';
+	if ($mobile)
+		$output .= '<b>Tupina\'o-meter</b><br />';
+	$output .= '<div class="slidecontainer">
+	  -10<input type="range" min="-10" max="10" value="0" class="slider" name="tupinarange" id="tupinarange">+10
+	</div>';
+	$output .= '<input type="checkbox" id="add_nihilism" name="add_nihilism" value="1">';
+	$output .= '<label for="add_nihilism">...ripauksella nihilismi&auml;</label><br>';
+	$output .= '<div class="help-tip">
+	    <p>Tupina\'o-meter&copy; :ll&auml; v&auml;henn&auml;t tai lis&auml;&auml;t peliin tupinaa ja jupinaa aiheuttavia elementtej&auml;.<br /><br />Ja jos todella haluat koetella k&auml;rsiv&auml;llisyytesi rajoja niin voit h&ouml;yst&auml;&auml; peli&auml; ripauksella nihilismi&auml; ja pienent&auml;&auml; rahaa ja vastavetoja tuovien toimintakorttien mahdollisuutta.</p>
+	</div>';
+	$output .= '</td>';
+
+	if ($mobile) {
+		/* On a mobile we end the row here */
+		$output .= '</tr><tr><td></td>';
+	}
+
+	/* ...Kapita cell: */
+	$output .= '<td>';
+	if ($mobile)
+		$output .= '<b>Kapita\'o-meter</b><br />';
+	$output .= '<div class="slidecontainer">
+	  -10<input type="range" min="-10" max="10" value="0" class="slider" name="kapitarange" id="kapitarange">+10
+	</div>';
+	$output .= '<div class="help-tip">
+	    <p>Kapita\'o-meter&copy; :ll&auml; voit muuttaa korttiarvontaa priorisoimaan raha- ja rahaa lis&auml;&auml;vi&auml; toimintakortteja.</p>
+	</div>';
+	$output .= '</td>';
+
+	/* End of the form table and form */
+	$output .= '</tr></table>';
+	$output .= '<input type="submit" value="Submit">';
+	$output .= '</form>';
+
+	return $output;
+}
 
 ?>
