60c08ed04f55100bc3f708dcaae7c42b73b4aa38
Matti Vaittinen <mazziesaccount@gmail.com>
2025-12-13 12:51:53 +0200

    sql: some old card table backup

--- web/cards.sql
+++ web/cards.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
 --
--- Host: mva-rohm    Database: dominion
+-- Host: localhost    Database: dominion
 -- ------------------------------------------------------
--- Server version	8.0.40
+-- Server version	8.0.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,6 +27,7 @@ CREATE TABLE `cards` (
   `dual_top_of_id` int unsigned DEFAULT '0',
   `dual_below_id` int unsigned DEFAULT '0',
   `name` varchar(255) NOT NULL,
+  `en_name` varchar(255) DEFAULT NULL,
   `expansion_id` int unsigned DEFAULT NULL,
   `type_id` int unsigned NOT NULL,
   `prizetype_id` int unsigned NOT NULL,
@@ -51,152 +52,8 @@ CREATE TABLE `cards` (
 
 LOCK TABLES `cards` WRITE;
 /*!40000 ALTER TABLE `cards` DISABLE KEYS */;
-INSERT INTO `cards` VALUES (1,0,0,'Rakennusmestari',11,2,2,4,0,0,0,0,0,0,0,0,0,0),(2,0,0,'Kaupunginosa',11,2,2,8,0,0,0,0,0,0,0,8,0,0),(3,0,0,'Ylipäällikkö',11,2,2,8,0,0,0,0,0,0,0,2,0,0),(4,0,0,'Kuninkaanseppä',11,2,2,8,5,0,0,0,0,0,0,0,0,0),(6,7,0,'Leiri',11,2,1,2,2,0,0,0,0,0,0,5,0,0),(7,0,6,'Ryöstösaalis',11,1,1,5,0,0,0,0,0,0,0,0,0,0),(8,9,0,'Patriisi',11,2,1,2,1,0,0,0,0,0,0,3,0,0),(9,0,8,'Markkinapaikka',11,2,1,5,1,0,0,0,0,0,0,4,0,1),(10,11,0,'Uudisasukkaat',11,2,1,2,1,0,0,0,0,0,0,3,0,0),(11,0,10,'Vilkas kylä',11,2,1,5,1,0,0,0,0,0,0,5,0,0),(12,13,0,'Katapultti',11,2,1,3,0,1,0,0,0,1,1,0,1,1),(13,0,12,'Kivet',11,1,1,4,0,0,0,0,0,0,0,0,0,0),(14,0,0,'Linnat',11,4,1,3,0,0,0,0,0,0,0,0,0,1),(15,0,0,'Vaunukisat',11,2,1,3,0,0,0,0,0,0,0,1,0,0),(16,0,0,'Lumoajatar',11,2,1,3,2,1,0,1,0,0,0,1,0,0),(17,0,0,'Maalaismarkkinat',11,2,1,3,0,0,0,0,1,0,0,0,0,2),(18,19,0,'Gladiaattori',11,2,1,3,0,0,0,0,0,0,0,0,0,2),(19,0,18,'Palkinto',11,1,2,16,0,0,0,0,0,0,0,0,0,0),(20,0,0,'Uhraus',11,2,1,4,1,0,0,0,0,1,0,2,0,1),(21,0,0,'Temppeli',11,2,1,4,0,0,0,0,1,1,0,0,0,0),(22,0,0,'Huvila',11,2,1,4,0,0,0,0,0,0,0,9,0,1),(23,0,0,'Arkisto',11,2,1,5,0,0,0,1,0,0,0,2,0,0),(24,0,0,'Pääoma',11,1,1,5,0,0,0,0,0,0,0,0,0,0),(25,0,0,'Onnenkalu',11,1,1,5,0,0,0,0,0,0,0,0,0,0),(26,0,0,'Kruunu',11,3,1,5,0,0,0,0,0,0,0,0,0,0),(27,0,0,'Foorumi',11,2,1,5,3,0,0,0,0,0,0,3,0,0),(28,0,0,'Henkijahti',11,2,1,5,3,0,0,0,1,0,0,0,0,0),(29,0,0,'Legioonalainen',11,2,1,5,0,1,0,0,0,0,0,0,1,3),(30,0,0,'Puutarhuri',11,2,1,5,1,0,0,0,0,0,0,2,0,0),(31,0,0,'Byrokraatti',1,2,1,4,0,1,0,0,0,0,0,0,0,0),(32,0,0,'Juhlat',1,2,1,5,0,0,0,0,0,0,0,4,0,2),(33,0,0,'Kaivos',1,2,1,5,0,0,0,0,0,1,0,0,0,0),(34,0,0,'Kansleri',1,2,1,3,0,0,0,0,0,0,0,0,0,2),(35,0,0,'Kappeli',1,2,1,2,0,0,0,0,0,1,0,0,0,0),(36,0,0,'Kellari',1,2,1,2,0,0,0,0,0,0,0,2,0,0),(37,0,0,'Kirjasto',1,2,1,5,3,0,0,0,0,0,0,0,0,0),(38,0,0,'Koronkiskuri',1,2,1,4,0,0,0,0,0,1,0,0,0,3),(39,0,0,'Kylä',1,2,1,3,1,0,0,0,0,0,0,4,0,0),(40,0,0,'Laboratorio',1,2,1,5,2,0,0,0,0,0,0,3,0,0),(41,0,0,'Metsuri',1,2,1,3,0,0,0,0,0,0,0,0,0,2),(42,0,0,'Muutostyö',1,2,1,4,0,0,0,0,0,1,0,0,0,0),(43,0,0,'Noita',1,2,1,5,2,1,0,0,0,0,1,0,0,0),(44,0,0,'Nostoväki',1,2,1,4,0,1,0,0,0,0,0,0,1,2),(45,0,0,'Pidot',1,2,1,4,0,0,0,0,0,1,0,0,0,0),(46,0,0,'Puutarha',1,4,1,4,0,0,0,0,0,0,0,0,0,0),(47,0,0,'Raatihuone',1,2,1,5,4,0,0,0,0,0,0,2,0,0),(48,0,0,'Seikkailija',1,2,1,6,1,0,0,0,0,0,0,0,0,0),(49,0,0,'Takomo',1,2,1,4,3,0,0,0,0,0,0,0,0,0),(50,0,0,'Tori',1,2,1,5,1,0,0,0,0,0,0,0,0,1),(51,0,0,'Työpaja',1,2,1,3,0,0,0,0,0,0,0,0,0,0),(52,0,0,'Vakooja',1,2,1,4,1,1,0,0,0,0,0,0,0,0),(53,0,0,'Vallihauta',1,2,1,2,2,0,1,0,0,0,0,0,0,0),(54,0,0,'Valtaistuinsali',1,2,1,4,0,0,0,0,0,0,0,3,0,0),(55,0,0,'Varas',1,2,1,4,0,1,0,0,0,1,0,3,0,0),(56,0,0,'Spurgutyö',17,2,1,4,0,0,0,0,0,1,0,0,0,0),(57,0,0,'Linnoitettu-kylä',17,2,1,4,0,0,0,0,0,0,0,4,0,0),(58,0,0,'Mustapörssi',17,2,1,3,0,0,0,0,0,0,0,0,0,0),(59,0,0,'Laina',5,1,1,3,0,0,0,0,0,1,0,0,0,0),(60,0,0,'Kauppareitti',5,2,1,3,0,0,0,0,1,1,0,0,0,2),(61,0,0,'Vartiotorni',5,2,1,3,1,0,1,0,0,1,0,0,0,0),(62,0,0,'Piispa',5,2,1,4,0,0,0,0,0,1,0,0,0,1),(63,0,0,'Monumentti',5,2,1,4,0,0,0,0,0,0,0,0,0,2),(64,0,0,'Louhos',5,1,1,4,0,0,0,0,0,0,0,1,0,0),(65,0,0,'Talismaani',5,1,1,4,0,0,0,0,0,0,0,0,0,0),(66,0,0,'Työläiskylä',5,2,1,4,1,0,0,0,0,0,0,4,0,0),(67,0,0,'Kaupunki',5,2,1,5,1,0,0,0,0,0,0,5,0,0),(68,0,0,'Hämärät varat',5,1,1,5,0,0,0,0,0,0,0,0,0,0),(69,0,0,'Tilitoimisto',5,2,1,5,0,0,0,0,0,0,0,0,0,0),(70,0,0,'Rahapaja',5,2,1,5,0,0,0,0,0,1,0,0,0,0),(71,0,0,'Petkuttaja',5,2,1,5,0,1,0,0,0,0,1,0,0,2),(72,0,0,'Rahvas',5,2,1,5,3,1,0,0,0,0,0,0,1,0),(73,0,0,'Sinetti',5,1,1,5,0,0,0,0,0,0,0,0,0,0),(74,0,0,'Holvi',5,2,1,5,2,0,0,0,0,0,0,1,0,1),(75,0,0,'Palkkio',5,1,1,5,0,0,0,0,0,0,0,0,0,0),(76,0,0,'Roistot',5,2,1,6,0,1,0,0,0,0,0,0,1,2),(78,0,0,'Aarre',5,1,1,6,0,0,0,0,0,0,0,0,0,0),(80,0,0,'Suurtori',5,2,1,6,1,0,0,0,0,0,0,3,0,2),(81,0,0,'Pankki',5,1,1,7,0,0,0,0,0,0,0,0,0,0),(82,0,0,'Laajennus',5,2,1,7,0,0,0,0,0,1,0,0,0,0),(83,0,0,'Ahjo',5,2,1,7,0,0,0,0,0,1,0,0,0,0),(84,0,0,'Kuninkaan Hovi',5,2,1,7,0,0,0,0,0,0,0,0,0,0),(85,0,0,'Kulkukauppias',5,2,1,8,1,0,0,0,0,0,0,3,0,1);
+INSERT INTO `cards` VALUES (1,0,0,'Rakennusmestari','Engineer',11,2,2,4,0,0,0,0,0,0,0,0,0,0),(2,0,0,'Kaupunginosa','City Quarter',11,2,2,8,0,0,0,0,0,0,0,8,0,0),(3,0,0,'Ylipäällikkö','Overlord',11,2,2,8,0,0,0,0,0,0,0,2,0,0),(4,0,0,'Kuninkaanseppä','Royal Blacksmith',11,2,2,8,5,0,0,0,0,0,0,0,0,0),(6,7,0,'Leiri','Encampment',11,2,1,2,2,0,0,0,0,0,0,5,0,0),(7,0,6,'Ryöstösaalis','Plunder',11,1,1,5,0,0,0,0,0,0,0,0,0,0),(8,9,0,'Patriisi','Patrician',11,2,1,2,1,0,0,0,0,0,0,3,0,0),(9,0,8,'Markkinapaikka','Emporium',11,2,1,5,1,0,0,0,0,0,0,4,0,1),(10,11,0,'Uudisasukkaat','Settlers',11,2,1,2,1,0,0,0,0,0,0,3,0,0),(11,0,10,'Vilkas kylä','Bustling Village',11,2,1,5,1,0,0,0,0,0,0,5,0,0),(12,13,0,'Katapultti','Catapult',11,2,1,3,0,1,0,0,0,1,1,0,1,1),(13,0,12,'Kivet','Rocks',11,1,1,4,0,0,0,0,0,0,0,0,0,0),(14,0,0,'Linnat',NULL,11,4,1,3,0,0,0,0,0,0,0,0,0,1),(15,0,0,'Vaunukisat','Chariot Race',11,2,1,3,0,0,0,0,0,0,0,1,0,0),(16,0,0,'Lumoajatar','Enchantress',11,2,1,3,2,1,0,1,0,0,0,1,0,0),(17,0,0,'Maalaismarkkinat','Farmers\' Market',11,2,1,3,0,0,0,0,1,0,0,0,0,2),(18,19,0,'Gladiaattori','Gladiator',11,2,1,3,0,0,0,0,0,0,0,0,0,2),(19,0,18,'Palkinto','Fortune',11,1,2,16,0,0,0,0,0,0,0,0,0,0),(20,0,0,'Uhraus','Sacrifice',11,2,1,4,1,0,0,0,0,1,0,2,0,1),(21,0,0,'Temppeli','Temple',11,2,1,4,0,0,0,0,1,1,0,0,0,0),(22,0,0,'Huvila','Villa',11,2,1,4,0,0,0,0,0,0,0,9,0,1),(23,0,0,'Arkisto','Archive',11,2,1,5,0,0,0,1,0,0,0,2,0,0),(24,0,0,'Pääoma','Capital',11,1,1,5,0,0,0,0,0,0,0,0,0,0),(25,0,0,'Onnenkalu','Charm',11,1,1,5,0,0,0,0,0,0,0,0,0,0),(26,0,0,'Kruunu','Crown',11,3,1,5,0,0,0,0,0,0,0,0,0,0),(27,0,0,'Foorumi','Forum',11,2,1,5,3,0,0,0,0,0,0,3,0,0),(28,0,0,'Henkijahti','Wild Hunt',11,2,1,5,3,0,0,0,1,0,0,0,0,0),(29,0,0,'Legioonalainen','Legionary',11,2,1,5,0,1,0,0,0,0,0,0,1,3),(30,0,0,'Puutarhuri','Groundskeeper',11,2,1,5,1,0,0,0,0,0,0,2,0,0),(31,0,0,'Byrokraatti','Bureaucrat',1,2,1,4,0,1,0,0,0,0,0,0,0,0),(32,0,0,'Juhlat','Festival',1,2,1,5,0,0,0,0,0,0,0,4,0,2),(33,0,0,'Kaivos','Mine',1,2,1,5,0,0,0,0,0,1,0,0,0,0),(34,0,0,'Kansleri','Chancellor',1,2,1,3,0,0,0,0,0,0,0,0,0,2),(35,0,0,'Kappeli','Chapel',1,2,1,2,0,0,0,0,0,1,0,0,0,0),(36,0,0,'Kellari','Cellar',1,2,1,2,0,0,0,0,0,0,0,2,0,0),(37,0,0,'Kirjasto','Library',1,2,1,5,3,0,0,0,0,0,0,0,0,0),(38,0,0,'Koronkiskuri','Moneylender',1,2,1,4,0,0,0,0,0,1,0,0,0,3),(39,0,0,'Kylä','Village',1,2,1,3,1,0,0,0,0,0,0,4,0,0),(40,0,0,'Laboratorio','Laboratory',1,2,1,5,2,0,0,0,0,0,0,3,0,0),(41,0,0,'Metsuri','Woodcutter',1,2,1,3,0,0,0,0,0,0,0,0,0,2),(42,0,0,'Muutostyö','Remodel',1,2,1,4,0,0,0,0,0,1,0,0,0,0),(43,0,0,'Noita','Witch',1,2,1,5,2,1,0,0,0,0,1,0,0,0),(44,0,0,'Nostoväki','Militia',1,2,1,4,0,1,0,0,0,0,0,0,1,2),(45,0,0,'Pidot','Feast',1,2,1,4,0,0,0,0,0,1,0,0,0,0),(46,0,0,'Puutarha','Gardens',1,4,1,4,0,0,0,0,0,0,0,0,0,0),(47,0,0,'Raatihuone','Council Room',1,2,1,5,4,0,0,0,0,0,0,2,0,0),(48,0,0,'Seikkailija','Adventurer',1,2,1,6,1,0,0,0,0,0,0,0,0,0),(49,0,0,'Takomo','Smithy',1,2,1,4,3,0,0,0,0,0,0,0,0,0),(50,0,0,'Tori','Market',1,2,1,5,1,0,0,0,0,0,0,0,0,1),(51,0,0,'Työpaja','Workshop',1,2,1,3,0,0,0,0,0,0,0,0,0,0),(52,0,0,'Vakooja','Spy',1,2,1,4,1,1,0,0,0,0,0,0,0,0),(53,0,0,'Vallihauta','Moat',1,2,1,2,2,0,1,0,0,0,0,0,0,0),(54,0,0,'Valtaistuinsali','Throne Room',1,2,1,4,0,0,0,0,0,0,0,3,0,0),(55,0,0,'Varas','Thief',1,2,1,4,0,1,0,0,0,1,0,3,0,0),(56,0,0,'Spurgutyö','Dismantle',17,2,1,4,0,0,0,0,0,1,0,0,0,0),(57,0,0,'Linnoitettu-kylä','Walled Village',17,2,1,4,0,0,0,0,0,0,0,4,0,0),(58,0,0,'Mustapörssi','Black Market',17,2,1,3,0,0,0,0,0,0,0,0,0,0),(59,0,0,'Laina','Loan',5,1,1,3,0,0,0,0,0,1,0,0,0,0),(60,0,0,'Kauppareitti','Trade Route',5,2,1,3,0,0,0,0,1,1,0,0,0,2),(61,0,0,'Vartiotorni','Watchtower',5,2,1,3,1,0,1,0,0,1,0,0,0,0),(62,0,0,'Piispa','Bishop',5,2,1,4,0,0,0,0,0,1,0,0,0,1),(63,0,0,'Monumentti','Monument',5,2,1,4,0,0,0,0,0,0,0,0,0,2),(64,0,0,'Louhos','Quarry',5,1,1,4,0,0,0,0,0,0,0,1,0,0),(65,0,0,'Talismaani','Talisman',5,1,1,4,0,0,0,0,0,0,0,0,0,0),(66,0,0,'Työläiskylä','Worker\'s Village',5,2,1,4,1,0,0,0,0,0,0,4,0,0),(67,0,0,'Kaupunki','City',5,2,1,5,1,0,0,0,0,0,0,5,0,0),(68,0,0,'Hämärät varat','Contraband',5,1,1,5,0,0,0,0,0,0,0,0,0,0),(69,0,0,'Tilitoimisto','Counting House',5,2,1,5,0,0,0,0,0,0,0,0,0,0),(70,0,0,'Rahapaja','Mint',5,2,1,5,0,0,0,0,0,1,0,0,0,0),(71,0,0,'Petkuttaja','Mountebank',5,2,1,5,0,1,0,0,0,0,1,0,0,2),(72,0,0,'Rahvas','Rabble',5,2,1,5,3,1,0,0,0,0,0,0,1,0),(73,0,0,'Sinetti','Royal Seal',5,1,1,5,0,0,0,0,0,0,0,0,0,0),(74,0,0,'Holvi','Vault',5,2,1,5,2,0,0,0,0,0,0,1,0,1),(75,0,0,'Palkkio','Venture',5,1,1,5,0,0,0,0,0,0,0,0,0,0),(76,0,0,'Roistot','Goons',5,2,1,6,0,1,0,0,0,0,0,0,1,2),(78,0,0,'Aarre','Hoard',5,1,1,6,0,0,0,0,0,0,0,0,0,0),(80,0,0,'Suurtori','Grand Market',5,2,1,6,1,0,0,0,0,0,0,3,0,2),(81,0,0,'Pankki','Bank',5,1,1,7,0,0,0,0,0,0,0,0,0,0),(82,0,0,'Laajennus','Expand',5,2,1,7,0,0,0,0,0,1,0,0,0,0),(83,0,0,'Ahjo','Forge',5,2,1,7,0,0,0,0,0,1,0,0,0,0),(84,0,0,'Kuninkaan Hovi','King\'s Court',5,2,1,7,0,0,0,0,0,0,0,0,0,0),(85,0,0,'Kulkukauppias','Peddler',5,2,1,8,1,0,0,0,0,0,0,3,0,1);
 /*!40000 ALTER TABLE `cards` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cardtype`
---
-
-DROP TABLE IF EXISTS `cardtype`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cardtype` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cardtype`
---
-
-LOCK TABLES `cardtype` WRITE;
-/*!40000 ALTER TABLE `cardtype` DISABLE KEYS */;
-INSERT INTO `cardtype` VALUES (1,'raha'),(2,'toiminto'),(3,'raha/toiminto'),(4,'sekalainen');
-/*!40000 ALTER TABLE `cardtype` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `expansion`
---
-
-DROP TABLE IF EXISTS `expansion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `expansion` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `expansion`
---
-
-LOCK TABLES `expansion` WRITE;
-/*!40000 ALTER TABLE `expansion` DISABLE KEYS */;
-INSERT INTO `expansion` VALUES (1,'Valtakunta'),(2,'Hovin Juonet'),(3,'Kaukaiset rannat'),(4,'Alkemia'),(5,'Nousukausi'),(6,'Elonkorjuu'),(7,'Rajaseudut'),(8,'Katovuodet'),(9,'Killat'),(10,'Seikkailut'),(11,'Keisarikunta'),(12,'Nocturne'),(13,'Renaissance'),(14,'Menagerie'),(15,'Allies'),(16,'Plunder'),(17,'Extras');
-/*!40000 ALTER TABLE `expansion` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `position`
---
-
-DROP TABLE IF EXISTS `position`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `position` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `pos` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `position`
---
-
-LOCK TABLES `position` WRITE;
-/*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (1,'itse'),(2,'vasemmalla puolella oleva'),(3,'oikealla puolella oleva'),(4,'vastapäätä oleva'),(5,'oikealla puolella ISTUVA'),(6,'vasemmalla puolella ISTUVA'),(7,'vasemmalla puolella SEISOVA'),(8,'oikealla puolella SEISOVA'),(9,'kauimpana oleva'),(10,'lähimpänä oleva');
-/*!40000 ALTER TABLE `position` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `prizetype`
---
-
-DROP TABLE IF EXISTS `prizetype`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `prizetype` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `prizetype`
---
-
-LOCK TABLES `prizetype` WRITE;
-/*!40000 ALTER TABLE `prizetype` DISABLE KEYS */;
-INSERT INTO `prizetype` VALUES (1,'raha'),(2,'velka');
-/*!40000 ALTER TABLE `prizetype` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `starter`
---
-
-DROP TABLE IF EXISTS `starter`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `starter` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `aloittaja` varchar(1024) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `starter`
---
-
-LOCK TABLES `starter` WRITE;
-/*!40000 ALTER TABLE `starter` DISABLE KEYS */;
-INSERT INTO `starter` VALUES (1,'Pelaaja jonka puhelin on kauimpana'),(2,'Viimeksi puhunut pelaaja'),(3,'Pelaaja joka nukkui pisimpään'),(4,'Pelaaja joka heräsi ensin'),(5,'Isokenkäisin pelaaja'),(6,'Vähähiuksisin pelaaja'),(7,'Viimeksi autossa istunut pelaaja'),(8,'Viimeksi ruokaa laittanut pelaaja'),(9,'Viimeksi jotain purrut pelaaja'),(10,'Pitkähiuksisin pelaaja'),(11,'Lyhin pelaaja'),(12,'Viimeksi reppua kantanut pelaaja'),(13,'Viimeksi televisiota katsonut pelaaja'),(14,'Viimeksi hammaslääkärissä käynyt pelaaja'),(15,'Vähiten tietokonetta käyttävä pelaaja'),(16,'Pieninenäisin pelaaja'),(17,'Punahuulisin pelaaja'),(18,'Siloposkisin pelaaja'),(19,'Pitkäpartaisin pelaaja'),(20,'Pelaaja joka on kauimpana synnyinpaikkaansa'),(21,'Pelaaja joka on lähimpänä vanhempiaan'),(22,'Viimeksi WC:ssä käynyt pelaaja'),(23,'Viimeksi jotain laulanaut/hyräillyt pelaaja'),(24,'Viimeksi ulkona käynyt pelaaja'),(25,'Pelaaja jonka puhelimessa on vähiten akkua jäljellä'),(26,'Pelaaja joka haukotteli viimeksi'),(27,'Pelaaja joka on viimeksi tehnyt käsitöitä'),(28,'Pelaaja jolla on eniten värejä sukissaan'),(29,'Pelaaja joka on käynyt viimeksi liikuntahallissa');
-/*!40000 ALTER TABLE `starter` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `suggestions`
---
-
-DROP TABLE IF EXISTS `suggestions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `suggestions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `ehdotus` varchar(1024) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `suggestions`
---
-
-LOCK TABLES `suggestions` WRITE;
-/*!40000 ALTER TABLE `suggestions` DISABLE KEYS */;
-INSERT INTO `suggestions` VALUES (1,'Pelaaja jolla on vahvimmat silmälasit päässään'),(2,'Pelaaja jolla on eniten koruja');
-/*!40000 ALTER TABLE `suggestions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -208,4 +65,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-29 16:07:13
+-- Dump completed on 2025-05-05 15:05:27
