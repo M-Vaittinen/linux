93a5813b07169e464fb1e400baef339793934233
Matti Vaittinen <mazziesaccount@gmail.com>
2025-12-13 12:35:32 +0200

    dominion: Add debug print

--- web/include/dominion_common.php
+++ web/include/dominion_common.php
@@ -1,8 +1,24 @@
 <?php
 
-function get_expansions($conn)
+$CARDTYPE_MONEY = 1;
+
+function debug_print($str)
 {
-	$query = "SELECT DISTINCT e.id, e.name FROM expansion AS e JOIN parsed_cards as pc WHERE e.id = pc.exp_id";
+	global $DBG;
+
+	if (!$DBG)
+		return;
+
+	echo "$str.<br />";
+}
+
+function get_expansions($conn, $include_nocards = true)
+{
+	if ($include_nocards)
+		$query = "SELECT DISTINCT e.id, e.name FROM expansion AS e JOIN parsed_cards as pc WHERE e.id = pc.exp_id";
+	else
+		$query = "SELECT DISTINCT e.id, e.name FROM expansion AS e JOIN parsed_cards as pc JOIN cards as c WHERE e.id = pc.exp_id AND c.expansion_id = e.id";
+
 	$result = mysqli_query($conn, $query);
 	if (!$result)
 		die("no expansions");
