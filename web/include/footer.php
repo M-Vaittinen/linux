da148e6daa894e86367fddddfbb96c5388f6bf06
Matti Vaittinen <mazziesaccount@gmail.com>
2025-12-17 14:35:36 +0200

    footer: drop the visitor counter
    I don't really like collecting information about the users. Furthermore,
    loading scripts from external sites may not be fast.
    
    It sure would be nice to know if this page is used - but the visitor
    counter is not really a good way in these times... Times when all kinds
    of bots and scrapers roam the web anyways. Knowing if an user is a real
    human or not is not straightforward.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- web/include/footer.php
+++ web/include/footer.php
@@ -14,8 +14,8 @@ mysqli_close($conn);
 
 echo '<hr style="height:10px;border-width:0;color:#d2691e;background-color:#d2691e">';
 echo '<table class="structure"><tr><td>';
-echo "<a href='http://www.freevisitorcounters.com'>Counters</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=57ceb84220fd0f19e28a1c0659163db3326e40d4'></script>";
-echo '<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1289001/t/4"></script>';
+//echo "<a href='http://www.freevisitorcounters.com'>Counters</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=57ceb84220fd0f19e28a1c0659163db3326e40d4'></script>";
+//echo '<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1289001/t/4"></script>';
 echo '</td><td>';
 echo "Copyright (c) 2025, Matti Vaittinen mazziesaccount (at) gmail com";
 echo '</td></tr></table>';
