c08d0ee8bc9f9bbfa8e49640d328e6582974fc06
Matti Vaittinen <mazziesaccount@gmail.com>
2025-12-18 16:11:24 +0200

    card.php: Support table-prefixes
    Idea was to allow the caller to pass the table-prefix used in SQL query,
    so that fields with same name from different tables could be separated.
    
    I am not really sure if this commit makes any sense. I don't think the
    table prefixes are really used in fetched rows(?). It was already too
    late when I hastly suffled (pun intended) this in.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- web/include/card.php
+++ web/include/card.php
@@ -67,33 +67,33 @@ class dom_card
 
 	public $weight		= 0;  // For weighted random selection
 
-	private function __populate($row)
+	private function __populate($row, $tableprefix)
 	{
-		$this->id = isset($row['id']) ? $row['id'] : null;
-		$this->dual_top_of_id = isset($row['dual_top_of_id']) ? $row['dual_top_of_id'] : null;
-		$this->dual_below_id = isset($row['dual_below_id']) ? $row['dual_below_id'] : null;
-		$this->name = isset($row['name']) ? $row['name'] : null;
-		$this->en_name = isset($row['en_name']) ? $row['en_name'] : null;
-		$this->prize = isset($row['prize']) ? $row['prize'] : null;
-		$this->type_id = isset($row['type_id']) ? $row['type_id'] : null;
-		$this->prizetype_id = isset($row['prizetype_id']) ? $row['prizetype_id'] : null;
-		$this->expansion_id = isset($row['expansion_id']) ? $row['expansion_id'] : null;
-		$this->drawcards = isset($row['drawcards']) ? $row['drawcards'] : null;
-		$this->buys = isset($row['buys']) ? $row['buys'] : 0;
-		$this->attack = isset($row['attack']) ? $row['attack'] : 0;
-		$this->defence = isset($row['defence']) ? $row['defence'] : 0;
-		$this->endure = isset($row['endure']) ? $row['endure'] : 0;
-		$this->gather = isset($row['gather']) ? $row['gather'] : 0;
-		$this->destroy = isset($row['destroy']) ? $row['destroy'] : 0;
-		$this->curse = isset($row['curse']) ? $row['curse'] : 0;
-		$this->tuhinakerroin = isset($row['tuhinakerroin']) ? $row['tuhinakerroin'] : null;
-		$this->dropcards = isset($row['dropcards']) ? $row['dropcards'] : null;
-		$this->actionmoney = isset($row['actionmoney']) ? $row['actionmoney'] : null;
-		$this->type_name = isset($row['type_name']) ? $row['type_name'] : null;
-		$this->prizetype_name = isset($row['prizetype_name']) ? $row['prizetype_name'] : null;
-		$this->expansion_name = isset($row['expansion_name']) ? $row['expansion_name'] : null;
-		$this->below_name = isset($row['below_name']) ? $row['below_name'] : null;
-		$this->above_name = isset($row['above_name']) ? $row['above_name'] : null;
+		$this->id = isset($row[$tableprefix.'id']) ? $row[$tableprefix.'id'] : null;
+		$this->dual_top_of_id = isset($row[$tableprefix.'dual_top_of_id']) ? $row[$tableprefix.'dual_top_of_id'] : null;
+		$this->dual_below_id = isset($row[$tableprefix.'dual_below_id']) ? $row[$tableprefix.'dual_below_id'] : null;
+		$this->name = isset($row[$tableprefix.'name']) ? $row[$tableprefix.'name'] : null;
+		$this->en_name = isset($row[$tableprefix.'en_name']) ? $row[$tableprefix.'en_name'] : null;
+		$this->prize = isset($row[$tableprefix.'prize']) ? $row[$tableprefix.'prize'] : null;
+		$this->type_id = isset($row[$tableprefix.'type_id']) ? $row[$tableprefix.'type_id'] : null;
+		$this->prizetype_id = isset($row[$tableprefix.'prizetype_id']) ? $row[$tableprefix.'prizetype_id'] : null;
+		$this->expansion_id = isset($row[$tableprefix.'expansion_id']) ? $row[$tableprefix.'expansion_id'] : null;
+		$this->drawcards = isset($row[$tableprefix.'drawcards']) ? $row[$tableprefix.'drawcards'] : null;
+		$this->buys = isset($row[$tableprefix.'buys']) ? $row[$tableprefix.'buys'] : 0;
+		$this->attack = isset($row[$tableprefix.'attack']) ? $row[$tableprefix.'attack'] : 0;
+		$this->defence = isset($row[$tableprefix.'defence']) ? $row[$tableprefix.'defence'] : 0;
+		$this->endure = isset($row[$tableprefix.'endure']) ? $row[$tableprefix.'endure'] : 0;
+		$this->gather = isset($row[$tableprefix.'gather']) ? $row[$tableprefix.'gather'] : 0;
+		$this->destroy = isset($row[$tableprefix.'destroy']) ? $row[$tableprefix.'destroy'] : 0;
+		$this->curse = isset($row[$tableprefix.'curse']) ? $row[$tableprefix.'curse'] : 0;
+		$this->tuhinakerroin = isset($row[$tableprefix.'tuhinakerroin']) ? $row[$tableprefix.'tuhinakerroin'] : null;
+		$this->dropcards = isset($row[$tableprefix.'dropcards']) ? $row[$tableprefix.'dropcards'] : null;
+		$this->actionmoney = isset($row[$tableprefix.'actionmoney']) ? $row[$tableprefix.'actionmoney'] : null;
+		$this->type_name = isset($row[$tableprefix.'type_name']) ? $row[$tableprefix.'type_name'] : null;
+		$this->prizetype_name = isset($row[$tableprefix.'prizetype_name']) ? $row[$tableprefix.'prizetype_name'] : null;
+		$this->expansion_name = isset($row[$tableprefix.'expansion_name']) ? $row[$tableprefix.'expansion_name'] : null;
+		$this->below_name = isset($row[$tableprefix.'below_name']) ? $row[$tableprefix.'below_name'] : null;
+		$this->above_name = isset($row[$tableprefix.'above_name']) ? $row[$tableprefix.'above_name'] : null;
 	}
 
 	public function get_id()
@@ -101,14 +101,14 @@ class dom_card
 		return $this->id;
 	}
 
-	private function check_row_has(array $row, array $keys)
+	private function check_row_has(array $row, array $keys, $tableprefix)
 	{
 		foreach($keys as $key)
-			if (!isset($row[$key]))
-				die($key.' Missing');
+			if (!isset($row[$tableprefix.$key]))
+				die($tableprefix.$key.' Missing');
 	}
 
-	public static function from_full_row(array $row)
+	public static function from_full_row(array $row, $tableprefix = '')
 	{
 		$card = new self();
 
@@ -116,33 +116,35 @@ class dom_card
 		 * Some cards don't have names in both languages.
 		 * If this is the case, just use the name we have.
 		 */
-		if (isset($row['name']) && !isset($row['en_name']))
-			$row['en_name'] = $row['name'];
+		if (isset($row[$tableprefix.'name']) && !isset($row[$tableprefix.'en_name']))
+			$row[$tableprefix.'en_name'] = $row[$tableprefix.'name'];
 
-		if (!isset($row['name']) && isset($row['en_name']))
-			$row['name'] = $row['en_name'];
+		if (!isset($row[$tableprefix.'name']) && isset($row[$tableprefix.'en_name']))
+			$row[$tableprefix.'name'] = $row[$tableprefix.'en_name'];
 
 		/* Check all required data is given */
 		$required_keys = array('id', 'name', 'en_name', 'dual_top_of_id', 'prize', 'type_id', 'prizetype_id', 'expansion_id', 'drawcards', 'tuhinakerroin', 'dropcards', 'actionmoney', 'type_name', 'prizetype_name', 'expansion_name');
-		$card->check_row_has($row, $required_keys);
+		$card->check_row_has($row, $required_keys, $tableprefix);
 
-		$card->__populate($row);
+		$card->__populate($row, $tableprefix);
 
 		return $card;
 	}
 
-	public static function from_partial_row(array $row)
+	public static function from_partial_row(array $row, $tableprefix='')
 	{
 		global $CARD_DEFAULT_WEIGH;
 
 		$card = new self();
 
-		if (!isset($row['id']) ||
-		    !isset($row['tuhinakerroin']) ||
-		    !isset($row['actionmoney']))
+		debug_print("from_partial_row: tableprefix: $tableprefix, checks like $tableprefix'id'");
+
+		if (!isset($row[$tableprefix.'id']) ||
+		    !isset($row[$tableprefix.'tuhinakerroin']) ||
+		    !isset($row[$tableprefix.'actionmoney']))
 			die('Info necessary for randomizing is missing');
 
-		$card->__populate($row);
+		$card->__populate($row, $tableprefix);
 		$card->weight = $CARD_DEFAULT_WEIGH;
 
 		return $card;
